c0fbd4f5bb066cc09011a56308516b6b60486f12
gqinghua <923063647@qq.com>
2021-02-01 20:56:36 +0800

    2021/1/24/14/55 add

--- src/crud/users.rs
+++ src/crud/users.rs
@@ -1,13 +1,13 @@
 use anyhow::Result;
-use diesel::{insert_into, RunQueryDsl};
+use diesel::{insert_into, RunQueryDsl, QueryDsl};
 use diesel::pg::PgConnection;
-use crate::models::model::{UPdateSysUser};
+use crate::models::model::{UPdateSysUser, SysUser};
 use crate::schemas::users::{UserCreate, UserUpdate};
+use super::super::schema::sys_user::dsl::{sys_user};
 
 
-
-pub fn update(ids: &i32,db: &PgConnection, obj_in: &UPdateSysUser) -> Result<User> {
-    let updated_user = diesel::update(obj_in).set(obj_in).get_result(db)?;
+pub fn update(ids: &i32,db: &PgConnection, UPdateSys: &UPdateSysUser) -> Result<SysUser> {
+    let updated_user = diesel::update(sys_user.find(ids)).set(UPdateSys).get_result(db);
     Ok(updated_user)
 }
 
