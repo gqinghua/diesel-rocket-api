c6f9dbb85a84aa5bcccfd31bc28bf629fac33d04
gqinghua <923063647@qq.com>
2021-02-02 16:44:59 +0800

    2021/1/24/14/55 add

--- src/schema.rs
+++ src/schema.rs
@@ -8,17 +8,27 @@ table! {
 }
 
 table! {
+    sys_role (id) {
+        id -> Int4,
+        name -> Varchar,
+        del -> Varchar,
+        create_date -> Varchar,
+        parent_id -> Varchar,
+    }
+}
+
+table! {
     sys_user (id) {
         id -> Int4,
         account -> Varchar,
         password -> Varchar,
-        name -> Varchar,
+        name ->Varchar,
         del -> Varchar,
-
     }
 }
 
 allow_tables_to_appear_in_same_query!(
     posts,
+    sys_role,
     sys_user,
 );
